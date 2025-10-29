802d0ea4719bd7fbe435a23ed44ecb80d68efe77
Stacey-Lee-hub <staceypietersen2@gmail.com>
2025-10-29 16:16:25 +0200

    index.js set up

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // express logic
+import express from 'express';
+import dotenv from 'dotenv';
+import {cors} from 'cors';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const port = process.env.PORT || 3005;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
